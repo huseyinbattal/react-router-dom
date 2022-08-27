1dfc162e44730034308495393ee76169bd593d6e
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-27 22:08:53 +0300

    Update ParametreView.js

--- src/views/ParametreView.js
+++ src/views/ParametreView.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 function ParametreView() {
+  const {id} = useParams()
+  
   return (
-    <div>Parametre Sayfası</div>
+    <div>Parametre Sayfası<br/>
+      Gelen Parametre {id}
+    </div>
   )
 }
 
