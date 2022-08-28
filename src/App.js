e996dde269df498268dfefc99838507cddb34933
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-28 12:03:14 +0300

    route

--- src/App.js
+++ src/App.js
@@ -4,25 +4,38 @@ import HomeView from "./views/HomeView";
 import UserWiew from "./views/UserWiew";
 import ParametreView from "./views/ParametreView";
 import QueryParamsViews from "./views/QueryParamsViews";
+import NewView from "./views/NewView";
+import SportsNewsView from "./views/SportsNewsView";
 
 function App() {
   return (
     <div className="App">
+      
+      <div style={{backgroundColor:"pink", padding:"10px",borderRadius:"50px",margin:"10px"}} >
       <Link to="/">Ana Sayfa</Link>
-      {" "}
+      <br/>
       <Link to="/kullanici">Kullanıcı</Link>
-      {" "}
+      <br/>
       <Link to="/parametre/abc-123">Parametre Sayfası</Link>
-      {" "}
+      <br/>
       <Link to="/queryparametre?w=1111x1121&w=9999">Query Sayfası</Link>
+      <br/>
+      <Link to="/haberler">Haberler</Link>
+      </div>
 
+<hr/>
 
+      <div style={{backgroundColor:"green", padding:"10px", color:"white",borderRadius:"50px",margin:"10px"}}>
       <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="/kullanici" element={<UserWiew />} />
         <Route path="/parametre/:id" element={<ParametreView/>} />
-        <Route path="/queryparametre" element={<QueryParamsViews/>} />
+        <Route path="/queryparametre" element={<QueryParamsViews />} />
+          <Route path="/haberler" element={<NewView />} >
+            <Route path="/sporhaberleri" element={<SportsNewsView/>} />
+        </Route>
       </Routes>
+    </div>
     </div>
   );
 }
---
+++ src/views/SportsNewsView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SportsNewsView() {
+  return (
+    <div>Spor Haberleri</div>
+  )
+}
+
+export default SportsNewsView
