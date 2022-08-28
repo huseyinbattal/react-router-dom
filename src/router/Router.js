ee16c35a7ffd51c973c5518fa974f1765e12145b
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-28 15:28:22 +0300

    Update Router.js

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,12 +1,16 @@
 import { useRoutes } from 'react-router-dom'
+import HomeView from '../views/HomeView'
 
 
 const Router = () => {
     const routes = useRoutes(
         [
+            {
+                path: "/",
+                element:<HomeView/>
+            },
             {},
-            {},
-            
+
         ]
     )
   
