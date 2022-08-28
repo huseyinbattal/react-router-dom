d306b6fdd8bb51c3acdab5d4d012800e5b3f19fd
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-28 15:26:08 +0300

    Update Router.js

---
+++ src/router/Router.js
@@ -0,0 +1,15 @@
+import { useRoutes } from 'react-router-dom'
+
+
+const Router = () => {
+    const routes = useRoutes(
+        [
+            {},
+            {},
+            
+        ]
+    )
+  
+}
+
+export default Router
