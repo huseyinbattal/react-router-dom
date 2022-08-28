8253f6652da89c043950a62834b465ee0a78e1fb
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-08-28 15:42:38 +0300

    Update Router.js

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,5 +1,7 @@
+import { Children } from 'react'
 import { useRoutes } from 'react-router-dom'
 import HomeView from '../views/HomeView'
+import NewView from '../views/NewView'
 import ParametreView from '../views/ParametreView'
 import QueryParamsViews from '../views/QueryParamsViews'
 import UserWiew from '../views/UserWiew'
@@ -21,8 +23,15 @@ const Router = () => {
                 element:<ParametreView/>
             },
             {
-                path: "/queryparametre",
+                path: "/queryparametre/",
                 element:<QueryParamsViews/>
+            },
+            {
+                path: "/haberler/",
+                element: <NewView />,
+                children:[
+
+                ]
             },
             {
                 path: "*",
